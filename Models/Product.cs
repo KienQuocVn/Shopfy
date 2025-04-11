336a18c8834675c83071940da78450cf0e06f329
Elliot <boscowong31@gmail.com>
2025-04-11 14:14:42 +0700

    Update more files

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,7 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace ECommerceAllInOne.Models
+namespace Shofy.Models
 {
     public class Product
     {
--- Models/User.cs
+++ Models/User.cs
@@ -1,24 +1,27 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
-public class User
+namespace Shofy.Models
 {
-    [Key]
-    public int Id { get; set; }
-    [Required, StringLength(50)]
-    public required string Username { get; set; }
-    [Required]
-    public required string PasswordHash { get; set; }
-    [Required, EmailAddress]
-    public required string Email { get; set; }
-    [Required]
-    public required string Role { get; set; }
-    public DateTime CreatedDate { get; set; } = DateTime.Now;
-    [Phone]
-    public string PhoneNumber { get; set; }
-    public string? FullName { get; set; }
-    public string? Address { get; set; }
-    public virtual ICollection<Order> Orders { get; set; }
-    public virtual ICollection<Cart> Carts { get; set; }
-    public virtual ICollection<Review> Reviews { get; set; }
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(50)]
+        public required string Username { get; set; }
+        [Required]
+        public required string PasswordHash { get; set; }
+        [Required, EmailAddress]
+        public required string Email { get; set; }
+        [Required]
+        public required string Role { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        [Phone]
+        public string PhoneNumber { get; set; }
+        public string? FullName { get; set; }
+        public string? Address { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+        public virtual ICollection<Cart> Carts { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
 }
